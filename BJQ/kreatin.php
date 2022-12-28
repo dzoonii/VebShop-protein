567c40d66479fce4bb5edfcdadd86710239e3fd0
Nikola Rogonjic <55810714+dzoonii@users.noreply.github.com>
2022-12-28 03:55:49 +0100

    Add files via upload

---
+++ BJQ/kreatin.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Gym Shop</title>
+  </head>
+  
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #003566;">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SajShop</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav col-md-11">
+        <a class="nav-link " aria-current="page" href="index.php">Home</a>
+        <a class="nav-link" href="proteini.php">Protein</a>
+        <a class="nav-link active" href="kreatin.php">Creatin</a>
+        
+      </div>
+      <div class="col-md-1 "  style="color: white;">
+        <a href="#" id="cart"><i class="fa fa-shopping-cart icon-color"> Korpa</i> <span class="badge icon-color"></span></a>
+      </div>
+    </div>
+  </div>
+
+</nav>
+
+  <div class="row odvoj">
+    <div class="col-md-1"></div>
+        <div class="card col-md-2" style="width: 14rem;">
+          <img src="slik1.jpg" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">CREA MASS 500G - YAMAMOTO NUTRITION/KREATIN</h5>
+            <p class="card-text">çena</p>
+            <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+            <input type="submit" class="btn btn-primary" value="Buy" id="69">
+          </div>
+        </div>
+    <div class="col-md-2"></div>
+    <div class="card col-md-2" style="width: 14rem;">
+      <img src="slik4.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">KREATIN (CREATINE) THE - 500G (KREATIN MONOHIDRAT)</h5>
+        <p class="card-text">cena</p>
+        <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+        <input type="submit" class="btn btn-primary" value="Buy" id="69">
+      </div>
+    </div>
+    <div class="col-md-2"></div>
+    <div class="card col-md-2" style="width: 14rem;">
+      <img src="slik3.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">KREATIN (CREATINE) THE - 300G (KREATIN MONOHIDRAT)</h5>
+        <p class="card-text">Cena</p>
+        <!--<a href="#" class="btn btn-primary">Go somewhere</a>-->
+        <input type="submit" class="btn btn-primary" value="Buy" id="69">
+      </div>
+    </div>
+    <div class="col-md-1"></div>
+</div>
+
+<!-- Remove the container if you want to extend the Footer to full width. -->
+<div class="container-fluid p-0">
+
+  <footer class="text-center text-lg-start" style="background-color: #003566;">
+    <div class="container d-flex justify-content-center py-5">
+      <button type="button" class="btn btn-lg btn-floating mx-2" style="background-color: #000814;">
+        <i class="fa fa-facebook-f icon-color"></i>
+      </button>
+      <button type="button" class="btn btn-lg btn-floating mx-2" style="background-color: #000814;">
+        <i class="fa fa-pinterest icon-color"></i>
+      </button>
+      <button type="button" class="btn btn-lg btn-floating mx-2" style="background-color: #000814;">
+        <i class="fa fa-instagram icon-color"></i>
+      </button>
+      <button type="button" class="btn btn-lg btn-floating mx-2" style="background-color: #000814;">
+        <i class="fa fa-twitter icon-color"></i>
+      </button>
+    </div>
+
+    <!-- Copyright -->
+    <div class="text-center text-white p-3" style="background-color: #001d3d;">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  
+</div>
+<!-- End of .container -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+  
+</body>
+</html>
